d64b7a5fab02e06fec21d65d8e81ddd61e90a12c
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2019-04-15 21:22:25 +0200

    Added methods to play a random sound

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,41 @@
 <template>
     <div id="app"></div>
 </template>
+
+<script>
+import {Howl, Howler} from 'howler';
+
+export default {
+    data() {
+        return {
+            path: '/sounds/',
+            sounds: [
+                '1152_1024.mp3',
+                '1280_1024.mp3',
+                '1280_1152.mp3',
+            ],
+        };
+    },
+    methods: {
+        getRandomSound() {
+            return this.sounds[Math.floor(Math.random() * this.sounds.length)];
+        },
+        playRandomSound() {
+            let soundFile = this.path + this.getRandomSound();
+            var sound = new Howl({
+                src: [soundFile],
+                autoplay: true,
+                loop: true,
+                volume: 1,
+                onend: function() {
+                    console.log('Finished!');
+                },
+            });
+            return sound;
+        },
+    },
+    mounted() {
+        this.playRandomSound();
+    },
+};
+</script>
